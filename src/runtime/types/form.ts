37eb274754df41044fba566da36e677d85c129a8
Eugen Istoc <eugenistoc@gmail.com>
2025-08-18 19:24:54 -0400

    refactor(form): remove unused error handling interfaces and related logic

--- src/runtime/types/form.ts
+++ src/runtime/types/form.ts
@@ -8,9 +8,6 @@ import type { Struct as SuperstructSchema } from 'superstruct'
 export interface Form<S extends FormSchema> {
   validate<T extends boolean>(opts?: { name?: keyof FormData<S, false> | (keyof FormData<S, false>)[], silent?: boolean, nested?: boolean, transform?: T }): Promise<FormData<S, T> | false>
   clear (path?: string): void
-  errors: Ref<FormError[]>
-  setErrors (errs: FormError[], name?: keyof FormData<S, false>): void
-  getErrors (name?: keyof FormData<S, false>): FormError[]
   submit (): Promise<void>
   disabled: ComputedRef<boolean>
   dirty: ComputedRef<boolean>
@@ -46,23 +43,7 @@ export type FormData<S extends FormSchema, T extends boolean = true> = T extends
 
 export type FormInputEvents = 'input' | 'blur' | 'change' | 'focus'
 
-export interface FormError<P extends string = string> {
-  name?: P
-  message: string
-}
-
-export interface FormErrorWithId extends FormError {
-  id?: string
-}
-
 export type FormSubmitEvent<T> = SubmitEvent & { data: T }
-
-export type FormValidationError = {
-  errors: FormErrorWithId[]
-  children?: FormValidationError[]
-}
-
-export type FormErrorEvent = SubmitEvent & FormValidationError
 
 export type FormEventType = FormInputEvents
 
@@ -104,23 +85,4 @@ export interface FormFieldInjectedOptions<T> {
   description?: string
   help?: string
   ariaId: string
-}
-
-export interface ValidateReturnSchema<T> {
-  result: T
-  errors: FormError[] | null
-}
-
-export class FormValidationException extends Error {
-  formId: string | number
-  errors: FormErrorWithId[]
-  children?: FormValidationException[]
-
-  constructor(formId: string | number, errors: FormErrorWithId[], childErrors?: FormValidationException[]) {
-    super('Form validation exception')
-    this.formId = formId
-    this.errors = errors
-    this.children = childErrors
-    Object.setPrototypeOf(this, FormValidationException.prototype)
-  }
 }
