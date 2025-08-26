d529e9da0724a19b70af480e10289e13ba19b47c
Eugen Istoc <eugenistoc@gmail.com>
2025-08-26 09:24:23 -0400

    chore: inject in color, highlight, and disabled

--- src/runtime/composables/useFormField.ts
+++ src/runtime/composables/useFormField.ts
@@ -4,10 +4,14 @@ import type { FormFieldProps } from '../types'
 import type { FormFieldInjectedOptions, FormGroupInjectedOptions } from '../types/form'
 import type { GetObjectField } from '../types/utils'
 import type { FormGroupProps } from '../components/FormGroup.vue'
+import { useFormContext } from '@formwerk/core'
 
 type Props<T> = {
   name?: string
   size?: GetObjectField<T, 'size'>
+  color?: GetObjectField<T, 'color'>
+  highlight?: boolean
+  disabled?: boolean
 }
 
 export const formFieldInjectionKey: InjectionKey<ComputedRef<FormFieldInjectedOptions<FormFieldProps>> | undefined> = Symbol('nuxt-ui.form-field')
@@ -15,14 +19,23 @@ export const formGroupInjectionKey: InjectionKey<ComputedRef<FormGroupInjectedOp
 export const formLoadingInjectionKey: InjectionKey<Readonly<Ref<boolean>>> = Symbol('nuxt-ui.form-loading')
 
 export function useFormField<T>(props?: Props<T>) {
+  const formContext = useFormContext()
+
+  const isDisabled = computed(() => formContext?.isDisabled.value)
+
   const formField = inject(formFieldInjectionKey, undefined)
   const formGroup = inject(formGroupInjectionKey, undefined)
 
   // Blocks the FormField injection to avoid duplicating events when nesting input components.
   provide(formFieldInjectionKey, undefined)
 
+  const hasError = computed(() => !!(formField?.value.hasError))
+
   return {
     name: computed(() => props?.name ?? formField?.value?.name),
-    size: computed(() => props?.size ?? formField?.value?.size ?? formGroup?.value?.size)
+    size: computed(() => props?.size ?? formField?.value?.size ?? formGroup?.value?.size),
+    color: computed(() => hasError.value ? 'error' : props?.color),
+    highlight: computed(() => hasError.value ? true : props?.highlight),
+    disabled: computed(() => isDisabled.value || props?.disabled)
   }
 }
